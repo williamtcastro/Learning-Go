ad3603c46c7141265e9f80328452d9f698d34c75
William <williamtdcastro@gmail.com>
2018-02-03 00:56:33 -0200

    Separe Files on folder, because the Editor is crazy

---
+++ Basics/First File/hello-world.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World and Welcome to Go!")
+	fmt.Println("Comments in Go is called using //,")
+	fmt.Println("Multi-Line comments are called using /* and */ to close them.")
+}
