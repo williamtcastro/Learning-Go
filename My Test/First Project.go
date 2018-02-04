842a4d082b92657d7d903edcd437a47efa8f046c
William <williamtdcastro@gmail.com>
2018-02-04 18:16:38 -0200

    Update #0401

---
+++ My Test/First Project.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	fmt.Println(add(1,2))
+}
+
+func add(num1 int,num2 int) (int, int)  {
+	
+	x := 1
+	num1 += x 
+	num2 += x
+
+	return num1, num2
+}
